d89d3fb32ce56946820b3c73dfb6bd19cc5db1a2
Rian Ribeiro Rodrigues <rianribeiroprogramador@gmail.com>
2021-09-20 17:31:40 -0300

    variables

--- js/scripts.js
+++ js/scripts.js
@@ -8,24 +8,30 @@ function imc() {
 
   if (name !== "" && height !== "" && weight !== "") {
     const imcValue = (weight / (height * height)).toFixed(1);
+    const underweight = imcValue < 18.5;
+    const idealWeight = imcValue < 25;
+    const slightlyOverweight = imcValue < 30;
+    const obesityFirstDegree = imcValue < 35
+    const obesitySecondDegree = imcValue < 40;
 
     let classification = "";
 
-    if (imcValue < 18.5) {
+    if ( underweight ) {
       classification = "abaixo do peso.";
-    } else if (imcValue < 25) {
+    } else if ( idealWeight ) {
       classification = "com o peso ideal. Parabéns!";
-    } else if (imcValue < 30) {
+    } else if ( slightlyOverweight ) {
       classification = "levemente acima do peso.";
-    } else if (imcValue < 35) {
+    } else if ( obesityFirstDegree ) {
       classification = "com obesidade Grau I.";
-    } else if (imcValue < 40) {
+    } else if ( obesitySecondDegree ) {
       classification = "com obesidade Grau II.";
     } else {
       classification = "com obesidade Grau III. Cuidado!";
     }
 
     result.textContent = `${name}, seu IMC é ${imcValue} e você está ${classification}`;
+    
   } else {
     result.textContent = "Preencha todos os campos!";
   }
